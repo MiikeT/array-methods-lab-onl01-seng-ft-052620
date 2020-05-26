f00feae3423c848d6e758f23426378ee1c79a3ad
Michael Takeuchi <miketaki@sbcglobal.net>
2020-05-26 18:52:39 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,17 +1,16 @@
 def using_include(array, element)
-  
+  array.include?(element)
 end
 
 def using_sort(array)
-
+  array.sort
 end
 
 def using_reverse(array)
-
+  array.reverse
 end
 
-def using_first(array)
-
+def using_fir  
 end
 
 def using_last(array)
